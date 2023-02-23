868a0def252e154f4006c00b0119394515ca3190
Wriddhi Hazra <wriddhihazra@gmail.com>
2023-02-23 16:58:10 +0530

    Edit employee fixed

--- pages/api/getAllUsers.js
+++ pages/api/getAllUsers.js
@@ -8,19 +8,7 @@ const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default async function handler(req, res) {
   const { data : users, error } = await supabase.from('employee').select('*')
-  
-  if (error) {
-    res.status(500).json({ error: error.message })
-  }
 
-  const modifiedUsers = await Promise.all(
-    users.map(async (user) => {
-      const {data: department} = await supabase.from('department').select('description').eq('department', user.department)
-      const { description } = department[0]
-      return { ...user, department: description }
-    })
-  )
-
-  res.status(200).json(modifiedUsers)
+  res.status(200).json(users)
   return
 }
